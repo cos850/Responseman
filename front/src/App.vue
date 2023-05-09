b067cdf2a1ee42eaf3e7f028b98b8d16a09c23b9
HyeRi Jeong <cos850@naver.com>
2023-05-09 22:54:32 +0900

    Detail 화면 추가 및 사이드 목록 변경

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-    <servers-sidebar></servers-sidebar>
+      <!-- header navbar -->
+      <nav class="navbar text-bg-dark">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                  <img src="public/logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                  <span class="text-light">Responseman</span>
+              </a>
+          </div>
+      </nav>
+
+      <div class="container-fluid">
+          <div class="row">
+              <servers-sidebar></servers-sidebar>
+              <detail-info></detail-info>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
   import ServersSidebar from "./components/ServersSidebar.vue";
+  import DetailInfo from "./components/DetailInfo.vue"
 
   export default {
       name: "app",
       components: {
           ServersSidebar,
+          DetailInfo
       },
   };
 </script>
---
+++ front/src/components/DetailInfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <main class="col-md-8">
+        <p class="text-center fs-2"> {{ server.id }}. {{ server.name }} </p>
+        <div>
+            <div class="mb-3 row">
+                <label for="input" class="col-sm-2 col-form-label">PORT</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="port">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="input" class="col-sm-2 col-form-label">Response Data</label>
+                <div class="col-sm-10">
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                server: {
+                    id: 1,
+                    name: 'test001',
+                    port: '8888',
+                    responseData: '테스트용 응답 데이터입니다.'
+                }
+            }
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<style>
+
+
+</style>
